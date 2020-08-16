1756c940cd6ab22d14ebdb87cb6d46ee58ff2508
Dires-Zeleke <60709815+Dires-Zeleke@users.noreply.github.com>
2020-08-16 01:26:59 -0700

    Add files via upload
    v1.0

---
+++ build/precache-manifest.bc14219602f3516ef26f31f0904230cb.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "637f3faf7a878a6e28429ff4a93ce90d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "534a9dd5937281f73505",
+    "url": "/static/css/2.1bd367ea.chunk.css"
+  },
+  {
+    "revision": "75c8404d01cf1f345975",
+    "url": "/static/css/main.ff5dd58e.chunk.css"
+  },
+  {
+    "revision": "534a9dd5937281f73505",
+    "url": "/static/js/2.9ee7ef78.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.9ee7ef78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75c8404d01cf1f345975",
+    "url": "/static/js/main.3efc9321.chunk.js"
+  },
+  {
+    "revision": "5bf9d4457c60f9e8fce0",
+    "url": "/static/js/runtime-main.13568670.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
